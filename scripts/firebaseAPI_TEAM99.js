339fce95c247e6ca4a98997b7962183705fb8b75
persephone-wong <persephone.w315@gmail.com>
2024-02-20 13:30:05 -0800

    Added demo contents

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyBMs5I6VQk1S8i8MXICUpqUapl56_kkvTc",
+    authDomain: "comp1800-202330-demo-aeffd.firebaseapp.com",
+    projectId: "comp1800-202330-demo-aeffd",
+    storageBucket: "comp1800-202330-demo-aeffd.appspot.com",
+    messagingSenderId: "1085351692308",
+    appId: "1:1085351692308:web:1b7406a1d566bb21a6bf01"  
 };
 
 //--------------------------------------------
