6a59870f0c16103a26ec90f251bee422334c93a4
persephone-wong <persephone.w315@gmail.com>
2024-03-19 13:24:23 -0700

    Add reviews

--- scripts/main.js
+++ scripts/main.js
@@ -40,55 +40,55 @@ function readQuote(day) {
 }
 readQuote("tuesday");        //calling the function
 
-//-----------------------------------------------
-// Create a "max" number of hike document objects
-//-----------------------------------------------
-function writeHikes() {
-    //define a variable for the collection you want to create in Firestore to populate data
-    var hikesRef = db.collection("hikes");
+// //-----------------------------------------------
+// // Create a "max" number of hike document objects
+// //-----------------------------------------------
+// function writeHikes() {
+//     //define a variable for the collection you want to create in Firestore to populate data
+//     var hikesRef = db.collection("hikes");
 
-    hikesRef.add({
-        code: "BBY01",
-        name: "Burnaby Lake Park Trail", //replace with your own city?
-        city: "Burnaby",
-        province: "BC",
-        level: "easy",
-				details: "A lovely place for lunch walk",
-        length: 10,          //number value
-        hike_time: 60,       //number value
-        lat: 49.2467097082573,
-        lng: -122.9187029619698,
-        last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
-    });
-    hikesRef.add({
-        code: "AM01",
-        name: "Buntzen Lake Trail", //replace with your own city?
-        city: "Anmore",
-        province: "BC",
-        level: "moderate",
-        details: "Close to town, and relaxing",
-        length: 10.5,      //number value
-        hike_time: 80,     //number value
-        lat: 49.3399431028579,
-        lng: -122.85908496766939,
-        last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
-    });
-    hikesRef.add({
-        code: "NV01",
-        name: "Mount Seymour Trail", //replace with your own city?
-        city: "North Vancouver",
-        province: "BC",
-        level: "hard",
-        details:  "Amazing ski slope views",
-        length: 8.2,        //number value
-        hike_time: 120,     //number value
-        lat: 49.38847101455571,
-        lng: -122.94092543551031,
-        last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 1, 2023"))
-    });
-}
+//     hikesRef.add({
+//         code: "BBY01",
+//         name: "Burnaby Lake Park Trail", //replace with your own city?
+//         city: "Burnaby",
+//         province: "BC",
+//         level: "easy",
+// 				details: "A lovely place for lunch walk",
+//         length: 10,          //number value
+//         hike_time: 60,       //number value
+//         lat: 49.2467097082573,
+//         lng: -122.9187029619698,
+//         last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+//     });
+//     hikesRef.add({
+//         code: "AM01",
+//         name: "Buntzen Lake Trail", //replace with your own city?
+//         city: "Anmore",
+//         province: "BC",
+//         level: "moderate",
+//         details: "Close to town, and relaxing",
+//         length: 10.5,      //number value
+//         hike_time: 80,     //number value
+//         lat: 49.3399431028579,
+//         lng: -122.85908496766939,
+//         last_updated: firebase.firestore.Timestamp.fromDate(new Date("March 10, 2022"))
+//     });
+//     hikesRef.add({
+//         code: "NV01",
+//         name: "Mount Seymour Trail", //replace with your own city?
+//         city: "North Vancouver",
+//         province: "BC",
+//         level: "hard",
+//         details:  "Amazing ski slope views",
+//         length: 8.2,        //number value
+//         hike_time: 120,     //number value
+//         lat: 49.38847101455571,
+//         lng: -122.94092543551031,
+//         last_updated: firebase.firestore.Timestamp.fromDate(new Date("January 1, 2023"))
+//     });
+// }
 
-writeHikes(1);
+// writeHikes(1);
 
 //------------------------------------------------------------------------------
 // Input parameter is a string representing the collection we are reading from
